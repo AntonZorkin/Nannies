dee20d235dd8eb5145174dfb23622c97b7d039c1
Anton Zorkin <zanton1978@gmail.com>
2026-02-25 11:01:31 +0100

    auth.ts

---
+++ src/services/auth.ts
@@ -0,0 +1,23 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "../firebase";
+
+export const register = (email: string, password: string) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const login = (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const logout = () => {
+  return signOut(auth);
+};
+
+export const subscribeToAuth = (callback: any) => {
+  return onAuthStateChanged(auth, callback);
+};
